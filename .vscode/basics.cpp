2719c279fb8e3c92416b962252d6a22ce29598ab
Govind Kaushik <govindkaushik1603@gmail.com>
2026-03-27 18:01:01 +0530

    Add basics.cpp

---
+++ .vscode/basics.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"hellio"<< endl;
+    cout<<"kaushikji"<< endl;
+    
+    int a;
+    int b;
+    cout<<"Enter a and b"<<endl;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+//Question 1:- 
+cout<<"Answers 1:-"<<endl; 
+int c;
+int d; 
+int e;
+cout<<"Enter c,d and e to print product"<<endl;
+cin>>c>>d>>e;
+cout<<c*d*e<<endl;
+
+// Question 2:-
+cout<<"Answer 2:-"<<endl;
+int g;
+int s;
+cout<<"Enter G & S - to print average."<<endl;
+cin>>g>>s;
+cout<<(g+s)/2<<endl;
+
+    return 0;
+}
