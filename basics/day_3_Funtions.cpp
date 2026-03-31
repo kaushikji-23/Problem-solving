50920c65276c0b9f291898c8690fcf22fc704e82
Govind Kaushik <govindkaushik1603@gmail.com>
2026-03-31 23:01:28 +0530

    Funtion problem solved

---
+++ basics/day_3_Funtions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// Functions :- 
+// Sum :- 
+// Return type 
+// Funtion name
+// Input type
+int number(int n)
+{
+    // body
+    int result  = 2*n;
+    return result;
+}
+int main()
+{
+    int m  = 10;
+    int result  = number(m);
+    cout<<result<<endl;
+    return 0;
+}
